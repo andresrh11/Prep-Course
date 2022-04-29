e4986c170fd7808147c81587725164d450debac4
andresrh11 <andresr10020@gmail.com>
2022-04-28 20:42:35 -0500

    First save HW2

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,12 +5,28 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  while ( x > y){
+    return x;
+  }
+  while ( y > x ){
+    return y;
+  }
+  if ( x == y ){
+    return x || y;
+
+  }
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  while (edad >= 18) {
+    return 'Allowed';
+  }
+  while (edad < 18){
+  return 'Not allowed';
+  }
 }
   
 function conection(status) {
@@ -19,6 +35,13 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+  while (status === 1){
+    return 'Online';
+  }
+  while (status === 2){
+    return 'Away';
+  }
+  return 'Offline';
 }
 
 function saludo(idioma) {
@@ -28,6 +51,16 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  for(var al = 'aleman'; idioma === al;){
+    return 'Guten Tag!';  
+  }
+    for(var man = 'mandarin'; idioma === man;){
+      return 'Ni Hao!';
+    }
+      for(var ing = 'ingles'; idioma === ing;){
+        return 'Hello!';
+      }
+  return 'Hola!'
 }
 
 function colors(color) {
